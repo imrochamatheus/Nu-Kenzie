2492a99f94a913de40f84521e276993d31e991be
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-04-04 13:23:13 -0300

    :bug: Corrigido cálculo do valor total

--- src/components/TotalMoney/index.jsx
+++ src/components/TotalMoney/index.jsx
@@ -1,19 +1,19 @@
 import "./totalmoney.css";
 
 const TotalMoney = ({ transactions, convertToBRL }) => {
-  const filterInputValues = ({ type }) => type === "recipe";
+  const calculateTotal = () => {
+    const total = transactions.reduce((acc, { value, type }) => {
+      return type === "recipe" ? acc + +value : acc - +value;
+    }, 0);
+
+    return total;
+  };
 
   return transactions.length ? (
     <div className="total-money__container">
       <h3>Valor Total</h3>
-      <h3 className="total-money__amount">
-        {convertToBRL(
-          transactions
-            .filter(filterInputValues)
-            .reduce((acc, { value }) => acc + +value, 0)
-        )}
-      </h3>
-      <p>O valor se refere às entradas</p>
+      <h3 className="total-money__amount">{convertToBRL(calculateTotal())}</h3>
+      <p>O valor se refere ao cálculo de entradas e saídas</p>
     </div>
   ) : (
     <></>
